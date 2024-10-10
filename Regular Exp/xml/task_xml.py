135bb52fd2b33e6f3bf480c6e9e01879884d1d24
Martin Fernandez <em.fernandez@outlook.com>
2024-10-10 13:56:33 -0400

    xml task and implementation of clasese

---
+++ Regular Exp/xml/task_xml.py
@@ -0,0 +1,24 @@
+import urllib.request
+import xml.etree.ElementTree as ET
+
+url = input('Enter location: ')
+if len(url) < 1 : 
+    url = 'http://py4e-data.dr-chuck.net/comments_42.xml'
+
+print('Retrieving', url)
+uh = urllib.request.urlopen(url)
+data = uh.read()
+print('Retrieved',len(data),'characters')
+tree = ET.fromstring(data)
+
+counts = tree.findall('.//count')
+nums = list()
+for result in counts:
+    # Debug print the data :)
+    nums.append(result)
+
+    print(result.text)
+summ=(int (i.text)for i in counts)   
+print('Count:', len(nums))
+print('Sum:', sum(summ))
+
